db88a0916cb4fe8b82033452e68d11ec901f35bf
Meldak <meldak.z@gmail.com>
2025-06-04 22:37:08 -0600

    space

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/meldak.github.io/"
+  base: "/meldak.github.io/"
 })
